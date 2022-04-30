d26b353d5bdf9d199a9c5d8b59434c1d2cfd3e8a
Andriy Morozov <morozov@ztu.edu.ua>
2022-04-30 21:39:06 +0300

    Демонстрацiя методiв життєвого циклу компонента

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -7,7 +7,29 @@ export class Clock extends React.Component {
             date: new Date()
         };
         this.tick = this.tick.bind(this);
+        console.log('constructor');
     }
+    static getDerivedStateFromProps(props, state) {
+        console.log('getDerivedStateFromProps');
+        return {
+            rnd : Math.random()
+        }
+    }
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate');
+        return document.querySelector('.clock').innerHTML;
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate');
+        console.log(document.querySelector('.clock').innerHTML);
+        console.log(snapshot);
+    }
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('shouldComponentUpdate');
+        console.log(nextState);
+        return nextState.rnd > 0.5;
+    }
+
     tick() {
         this.setState(
             {
@@ -15,7 +37,9 @@ export class Clock extends React.Component {
             }
         );
     }
+
     componentDidMount() {
+        console.log('componentDidMount');
         /*this.timerId = setInterval(
             () => {
                 this.tick();
@@ -25,10 +49,12 @@ export class Clock extends React.Component {
     }
 
     componentWillUnmount() {
+        console.log('componentWillUnmount');
         clearInterval(this.timerId);
     }
 
     render() {
+        console.log('render');
         return (
             <div className="clock">{this.state.date.toLocaleTimeString()}</div>
         );
